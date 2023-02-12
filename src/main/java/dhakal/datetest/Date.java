ef9416cef49a7f28ca2092bece37d1666215577a
tikad <tikad@Tika.home>
2023-02-12 12:39:28 -0500

    tabular12

---
+++ src/main/java/dhakal/datetest/Date.java
@@ -0,0 +1,57 @@
+package dhakal.datetest;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tikad
+ */
+
+public class Date {
+    private int month;
+    private int day;
+    private int year;
+    
+    public Date (int month, int day, int year){
+     this.month = month;
+     this.day = day;
+     this.year = year;
+     
+        }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void displayDate(){
+        
+     System.out.println(month+ "/" +day+ "/" + year);
+        
+    }
+
+
+
+}
---
+++ src/main/java/dhakal/datetest/DateTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package dhakal.datetest;
+
+
+
+/**
+ *
+ * @author tikad
+ */
+public class DateTest {
+
+    public static void main(String[] args) {
+        Date date = new  Date(8, 31,1978);
+        //Date date2 = new  Date(1, 5,1978);
+//        System.out.println("Month:" + date.getMonth());
+//        System.out.println("Day:" + date.getDay());
+//        System.out.println("Year:" + date.getYear());
+//        System.out.print("Date");
+        date.displayDate();
+        //date2.displayDate();
+        
+    }
+}
